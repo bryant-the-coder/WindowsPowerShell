d961976e89fab05526c119277e3a1009675811f3
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-04 22:19:51 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,8 @@ Invoke-Expression -Command $(gh completion -s powershell | Out-String)
 
 # Delete if you dont want
 Import-Module Terminal-Icons
-Import-Module posh-git
-Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\bryant.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+# Import-Module oh-my-posh
+# $omp_config = Join-Path $PSScriptRoot ".\bryant.omp.json"
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Invoke-Expression (&starship init powershell)
